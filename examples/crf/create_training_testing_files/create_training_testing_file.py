7193ff67d06eb44b32c8fe91ca40945d22c5f575
Truong An Nguyen <62927494+yiimnta@users.noreply.github.com>
2021-01-25 19:14:52 +0100

    Update create_training_testing_file.py

--- examples/crf/create_training_testing_files/create_training_testing_file.py
+++ examples/crf/create_training_testing_files/create_training_testing_file.py
@@ -9,5 +9,5 @@ path = '../../../examples/crf/create_training_testing_files/'
 ##crate Training File
 pro.exportCRFTraining('../../../data/xml/18001.xml', path + 'output/18001_train_file.txt');
 
-##crate Training File
+##crate Testing File
 pro.exportCRFTesting('../../../data/xml/18002.xml', path + 'output/18002_test_file.txt');
